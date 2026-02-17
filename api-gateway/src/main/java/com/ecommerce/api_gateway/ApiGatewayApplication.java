9380298d349a4390d0a1a82c5f11f1dc97087dfd
aryantyagi10 <tyagiaryan104@gmail.com>
2026-02-18 01:48:25 +0530

    JWT Authentication at Gateway

--- api-gateway/src/main/java/com/ecommerce/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/ecommerce/api_gateway/ApiGatewayApplication.java
@@ -1,5 +1,6 @@
 package com.ecommerce.api_gateway;
 
+import com.ecommerce.api_gateway.filter.AuthenticationFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -16,14 +17,20 @@ public class ApiGatewayApplication {
 	}
 
     @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder, AuthenticationFilter authFilter){
+
+        // We create a new Config object for the filter
+        AuthenticationFilter.Config config = new AuthenticationFilter.Config();
+
         return builder.routes()
                 // Route 1: Product Service
                 .route("product-service", r -> r.path("/api/product/**")
+                        .filters(f -> f.filter(authFilter.apply(config)))
                         .uri("lb://product-service")) // <--- Lowercase matches Eureka better
 
                 // Route 2: Order Service
                 .route("order-service", r -> r.path("/api/order/**")
+                        .filters(f -> f.filter(authFilter.apply(config)))
                         .uri("lb://order-service"))
 
                 // Route 3: User Service
--- api-gateway/src/main/java/com/ecommerce/api_gateway/filter/AuthenticationFilter.java
+++ api-gateway/src/main/java/com/ecommerce/api_gateway/filter/AuthenticationFilter.java
@@ -1,4 +1,89 @@
 package com.ecommerce.api_gateway.filter;
 
-public class AuthenticationFilter {
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.apache.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public AuthenticationFilter(){
+        super(Config.class);
+    }
+
+
+    @Override
+    public GatewayFilter apply(Config config){
+        return (exchange, chain) -> {
+            // 1. Check Header
+            if(!exchange.getRequest()
+                    .getHeaders()
+                    .containsHeader(HttpHeaders.AUTHORIZATION)){
+                throw new RuntimeException("Missing Authorization Header");
+            }
+
+            // 2. Extract Token
+            String authHeader = exchange.getRequest()
+                    .getHeaders()
+                    .getFirst(HttpHeaders.AUTHORIZATION);
+
+            if(authHeader != null && authHeader.startsWith("Bearer ")){
+                authHeader = authHeader.substring(7);
+            }
+
+            // 3. Validate
+            try {
+                validateToken(authHeader);
+            } catch (Exception e){
+                throw new RuntimeException("Unauthorized Access");
+            }
+
+            // 4. Forward
+            return chain.filter(exchange);
+        };
+    }
+
+
+    private void validateToken(String token){
+        Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token);  //This is where actual validation happens
+    }
+
+
+    private Key getSignKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public static class Config{ }
 }
+
+
+
+
+
+/*
+--  .parserClaimsJws(token) - This where the actual validation happens
+                              Internally parser does:
+                                1. Split token into 3 parts
+                                2. Base64 decode header & payload
+                                3. Extract signature
+                                4. Recalculate signature using secret
+                                5. Compare both signature
+                                6. Check expiration
+                                7. If valid -> return Claims
+                                8. If invalid -> throw exception
+ */
